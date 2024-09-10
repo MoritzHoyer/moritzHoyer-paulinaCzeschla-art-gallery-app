5d5a4c0626bc168976bbe4c9f537e959deac221f
Moritz Hoyer <contact@moritzhoyer.de>
2024-09-10 19:53:56 +0200

    implement user story 1: List of all Art Pieces. Display all art pieces as list. Display each art piece’s image, title, and artist. Fetch all art pieces with SWR in page/index.js. Create components: ArtPieces, ArtPiecesPreview.

---
+++ components/ArtPiecePreview/index.js
@@ -0,0 +1,18 @@
+// Importiere die Next.js's Image-Komponente, um optimierte Bilder darzustellen.
+import Image from "next/image";
+
+// ArtPiecePreview-Komponente: Diese Komponente zeigt ein einzelnes Kunststück an.
+// Es nimmt die 'image', 'title', und 'artist'-Props, die von der ArtPieces-Komponente übergeben wurden.
+export default function ArtPiecePreview({ image, title, artist }) {
+  return (
+    <div>
+      {/* Verwende die Image-Komponente von Next.js, um das Bild des Kunststücks darzustellen.
+          Die Größe des Bildes ist hier auf 200x200 Pixel festgelegt. */}
+      <Image src={image} alt={title} width={200} height={200} priority />
+      {/* Zeige den Titel des Kunststücks */}
+      <h2>{title}</h2>
+      {/* Zeige den Namen des Künstlers */}
+      <p>{artist}</p>
+    </div>
+  );
+}
