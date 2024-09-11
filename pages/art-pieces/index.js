4f7a0c12b7569b15788804d91774bb3f3edb4aac
Moritz Hoyer <contact@moritzhoyer.de>
2024-09-11 15:40:31 +0200

    fix import of ArtPieces

--- pages/art-pieces/index.js
+++ pages/art-pieces/index.js
@@ -1,5 +1,5 @@
 // Importiere die ArtPiecePreview-Komponente, die jedes einzelne Kunststück rendern wird, und Next.js's Image-Komponente.
-import ArtPieces from "./components/ArtPieces/index.js";
+import ArtPieces from "../../components/ArtPieces/index.js";
 
 export default function ArtPiecesPage({ data }) {
   return <ArtPieces pieces={data} />;
